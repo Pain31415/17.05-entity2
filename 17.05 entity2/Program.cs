8154b59868a9229ff670c351a93d51b9e61fc09b
Chernii Vladyslav Ihorovych <Cherniy412@ukr.net>
2024-05-17 19:39:42 +0300

    Add project files.

---
+++ 17.05 entity2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _17._05_entity2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
